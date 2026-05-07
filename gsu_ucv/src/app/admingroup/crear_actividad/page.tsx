b0093a59787f26f80140fe92250bb0282b9b8bc6
splintx14 <ronaldmore1424@gmail.com>
2026-05-07 18:11:08 -0400

    haber si funciona

--- gsu_ucv/src/app/admingroup/crear_actividad/page.tsx
+++
@@ -1,5 +0,0 @@
-import CrearActividadForm from "@/components/formularios/crear-actividad-form";
-
-export default function CrearActividadPage() {
-  return <CrearActividadForm />;
-}
--- gsu_ucv/src/app/admingroup/page.tsx
+++
@@ -1,6 +0,0 @@
-// /app/admingroup/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function AdminRedirectPage() {
-  redirect('/admingroup/dashboard');
-}
