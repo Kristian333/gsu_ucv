4c40f849abf31105c1acc17f6b9265bb6e83c93f
Kristian Ferreira <kristiandc3@gmail.com>
2026-06-09 23:02:42 -0400

    Maqueta para mostrar

--- src/components/formularios/crear-grupo-form.tsx
+++ src/components/formularios/crear-grupo-form.tsx
@@ -483,7 +483,7 @@ export default function CrearGrupoForm() {
 
         {/* Proyecto PDF */}
         <FormControl isRequired>
-          <FormLabel>PROYECTO DEL GRUPO (PDF)</FormLabel>
+          <FormLabel>PROYECTO DEL GRUPO (.PDF)</FormLabel>
           <Input type="file" accept="application/pdf" onChange={handleProyecto} />
         </FormControl>
 
